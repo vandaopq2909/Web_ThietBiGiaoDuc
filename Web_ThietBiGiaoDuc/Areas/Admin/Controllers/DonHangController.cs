d26d164fe8919914df44860a8c29dd0639031853
Hungi03 <hunghung2k33@gmail.com>
2024-11-12 21:39:40 +0700

    pull main

---
+++ Web_ThietBiGiaoDuc/Areas/Admin/Controllers/DonHangController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web_ThietBiGiaoDuc.Areas.Admin.Controllers
+{
+    public class DonHangController : Controller
+    {
+        // GET: Admin/DonHang
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Them() { return View(); }
+        public ActionResult Sua() { return View(); }
+        public ActionResult ChiTiet() { return View(); }
+    }
+}
