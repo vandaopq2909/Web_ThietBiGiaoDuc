898e5a2aa7e51aa83f48599dc7bb8e4616733b6c
Hungi03 <hunghung2k33@gmail.com>
2024-11-13 20:46:25 +0700

    Add Model

---
+++ Web_ThietBiGiaoDuc/Models/DanhMucModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Web_ThietBiGiaoDuc.Models
+{
+    public class DanhMucModel
+    {
+        public DanhMucModel() 
+        {
+            MaDM= "DM" + Guid.NewGuid().ToString("N").Substring(0, 8);
+        }
+        public string MaDM { get; set; }
+        public string TenDM {  get; set; }
+        public string TrangThai { get; set; }
+
+    }
+}
---
+++ Web_ThietBiGiaoDuc/Models/ThuongHieuModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Web_ThietBiGiaoDuc.Models
+{
+    public class ThuongHieuModel
+    {
+        public ThuongHieuModel()
+        {
+            MaThuongHieu= "TH" + Guid.NewGuid().ToString("N").Substring(0, 8);
+        }
+        public string MaThuongHieu { get; set; }
+        public string TenThuongHieu { get; set; }
+        public string TrangThai {  get; set; }
+    }
+}
