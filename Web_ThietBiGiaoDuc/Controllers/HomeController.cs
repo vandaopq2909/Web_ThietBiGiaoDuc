7770fbd111fbc975b29b279388877363f30c2e37
Trần Văn Đảo <vandaopq@gmail.com>
2024-11-23 23:07:01 +0700

    update home page

--- Web_ThietBiGiaoDuc/Controllers/HomeController.cs
+++ Web_ThietBiGiaoDuc/Controllers/HomeController.cs
@@ -3,16 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Web_ThietBiGiaoDuc.Models;
 
 namespace Web_ThietBiGiaoDuc.Controllers
 {
+    public class SanPhamVM
+    {
+        public string MaSP { get; set; }
+        public string TenSanPham { get; set; }
+        public double Gia { get; set; }
+        public string img { get; set; }
+    }
     public class HomeController : Controller
     {
         public ActionResult Index()
         {
+            DatabaseContext db = new DatabaseContext();
+
+            // Lấy tối đa 4 sản phẩm có trạng thái 'hoatdong' và hình ảnh đầu tiên
+            ViewBag.listSPNoiBat = db.sanPhams
+                .Where(sp => sp.TrangThai == "hoatdong")  // Lọc sản phẩm có trạng thái 'hoatdong'
+                .Take(4)                                  // Lấy tối đa 4 sản phẩm
+                .Select(sp => new SanPhamVM
+                {
+                    MaSP = sp.MaSP,
+                    TenSanPham = sp.TenSanPham,
+                    Gia = sp.Gia,
+                    img = sp.HinhAnhs.Select(h => h.TenHinhAnh).FirstOrDefault()
+                }).ToList();
+
             return View();
         }
-
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
