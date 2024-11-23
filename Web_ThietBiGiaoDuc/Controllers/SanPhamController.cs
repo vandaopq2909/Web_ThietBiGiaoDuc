1b650f56efa79acee8247b14d547111855fe5e2d
Chiêm Hồng Hưng <154119588+hungi03@users.noreply.github.com>
2024-11-24 00:01:05 +0700

    update Product Detail 2

--- Web_ThietBiGiaoDuc/Controllers/SanPhamController.cs
+++ Web_ThietBiGiaoDuc/Controllers/SanPhamController.cs
@@ -3,19 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Web_ThietBiGiaoDuc.Models;
 
 namespace Web_ThietBiGiaoDuc.Controllers
 {
     public class SanPhamController : Controller
     {
         // GET: SanPham
+        DatabaseContext db = new DatabaseContext();
         public ActionResult Index()
         {
             return View();
-        }       
-        public ActionResult ChiTiet()
+        }
+        public ActionResult ChiTiet(string masp)
         {
-            return View();
+            SanPham sanPham = db.sanPhams.FirstOrDefault(sp => sp.MaSP == masp);
+
+            //List<SANPHAMCACANH> listsp = db.SANPHAMCACANHs.Where(x => x.LoaiCaID == sp.LoaiCaID)
+            //                                                .Take(4)
+            //                                                .ToList();
+            //ViewBag.listsp = listsp;
+            List<HinhAnh> lstha = db.hinhAnhs.Where(x => x.MaSP == masp).ToList();
+            ViewBag.lstHA = lstha;
+
+            return View(sanPham);
         }
     }
 }
