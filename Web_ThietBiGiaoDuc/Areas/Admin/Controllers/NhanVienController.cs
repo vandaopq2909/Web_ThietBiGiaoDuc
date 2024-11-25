92115a6dbed34312426e125e85e97fde22efe15a
Chiêm Hồng Hưng <154119588+hungi03@users.noreply.github.com>
2024-11-25 15:56:02 +0700

    Update login Admin

--- Web_ThietBiGiaoDuc/Areas/Admin/Controllers/NhanVienController.cs
+++ Web_ThietBiGiaoDuc/Areas/Admin/Controllers/NhanVienController.cs
@@ -17,5 +17,40 @@ namespace Web_ThietBiGiaoDuc.Areas.Admin.Controllers
             var t = db.nhanViens.ToList();
             return View();
         }
+        public ActionResult DangNhap() { return View(); }
+        [HttpPost]
+        public ActionResult DangNhap(NhanVien nhanVien)
+        {
+            if (nhanVien != null)
+            {
+                DatabaseContext db = new DatabaseContext();
+                NhanVien nv = db.nhanViens.Where(u => u.TenDangNhap == u.TenDangNhap).FirstOrDefault();
+                if (nv != null)
+                {
+                    if (BCrypt.Net.BCrypt.Verify(nhanVien.MatKhau, nv.MatKhau))
+                    {
+                        HttpCookie authCookie = new HttpCookie("auth", nv.TenDangNhap);
+                        HttpCookie roleCookie = new HttpCookie("role", nv.Quyen.TenQuyen);
+                        Response.Cookies.Add(authCookie);
+                        Response.Cookies.Add(roleCookie);
+                        if (nv.Quyen.TenQuyen == "quanli")
+                        {
+                            return RedirectToAction("Index", "Home", new { area = "Admin" });
+                        }
+                        else
+                        {
+                            return RedirectToAction("Index", "Products");
+                        }
+
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError("MatKhau", "Mật khẩu không đúng");
+                }
+
+            }
+            return View();
+        }
     }
 }
